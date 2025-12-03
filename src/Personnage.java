212fb60469747fd4b90d6458becbd326182853ea
whaugutin <whitchyaugustin@gmail.com>
2025-12-03 09:40:42 -0500

    Commit of the class Personnage

--- src/Personnage.java
+++ src/Personnage.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
-
 public class Personnage {
+    private String nom;
+    private int vie;
+    private int attaque;
+
+    // Constructor
+    public Personnage(String nom, int vie, int attaque) {
+        this.nom = nom;
+        this.vie = vie;
+        this.attaque = attaque;
+    }
+
+    // Getters and Setters
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getVie() {
+        return vie;
+    }
+
+    public void setVie(int vie) {
+        this.vie = vie;
+    }
+
+    public int getAttaque() {
+        return attaque;
+    }
+
+    public void setAttaque(int attaque) {
+        this.attaque = attaque;
+    }
+
+    // Methods
+    public void attaquer(Personnage personnage){
+        personnage.setVie(personnage.getVie() - 20);
+    }
+
+    public boolean estVivant(){
+        return this.vie != 0;
+    }
+
+    public String afficherEtat(){
+        return ("Nom : " + this.nom + "\nVie : " + this.vie);
+    }
 }

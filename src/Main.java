cbacbff349eb33f194ebfb0c6be9405b11706527
whaugutin <whitchyaugustin@gmail.com>
2025-12-08 15:02:27 -0500

    rename gatherPersonnage method createPersonnage

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,17 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static Personnage[] gatherPersonnages(){
+    public static Personnage[] createPersonnage(){
         Scanner sc = new Scanner(System.in);
         Personnage[] personnagesForPlayer = new Personnage[3];
         for(int i = 0; i < 3; i++){
             System.out.print("Nom du personnage " + (i+1) + " : ");
             String name = sc.nextLine();
-            personnagesForPlayer[i] = new  Personnage(name);
-//            personnagesForPlayer[i].setVie(100);
-//            personnagesForPlayer[i].setAttaque(20);
+            personnagesForPlayer[i] = new Personnage(name);
         }
         return personnagesForPlayer;
     }
 
-//    public static void displayPersonnageInfo(Personnage[] personnage){
-//        for(int i = 0; i < 3; i++) {
-//            System.out.println("Nom du personnage : " + personnage[i].getNom());
-//            System.out.println("Vie : " + personnage[i].getVie());
-//            System.out.println("Attaque : " + personnage[i].getAttaque());
-//        }
-//    }
 
     public static void main(String[] args) {
         System.out.println("Bienvenue dans le jeu combat !\n");
@@ -33,12 +24,13 @@ public class Main {
 
         System.out.println();
         System.out.println(nameOfPLayer1 + ", crée ton équipe");
-        Personnage[] personnagesForPlayer1 = gatherPersonnages();
-
+        Personnage[] personnagesForPlayer1 = createPersonnage();
 
         System.out.println();
         System.out.println(nameOfPLayer2 + ", crée ton équipe");
-        Personnage[] personnagesForPlayer2 = gatherPersonnages();
+        Personnage[] personnagesForPlayer2 = createPersonnage();
+
+
 
 
     }
